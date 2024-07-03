6586204c6b5895bf8c2329e332e506bf2ff9fd35
minh <vocasony@gmail.com>
2024-07-03 16:39:17 +0700

    changed

--- event-loop-examples/read-file.js
+++ event-loop-examples/read-file.js
@@ -1,6 +1,6 @@
 const {readFile}=require('fs')
 console.log('started a first task')
-readFile('../content/first.txt','utf8',(err,result)=>{
+readFile('./content/first.txt','utf8',(err,result)=>{
     if(err){
         console.log(err)
         return
